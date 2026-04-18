f890ef48cb24d4074aeb98cb60217ef690e04001
Potapenko Max <mpotapenko.mail@gmail.com>
2026-04-19 00:46:58 +0300

    add/order_manager

---
+++ OrderManager/OrderManager/Models/Order.cs
@@ -0,0 +1,17 @@
+namespace OrderManager.Models;
+
+public sealed class Order
+{
+    public string ProductName { get; }
+    public int  Quantity { get; }
+    public string CustomerName { get; }
+    public string Address { get; }
+
+    internal Order(string productName, int quantity, string customerName, string address)
+    {
+        ProductName = productName;
+        Quantity = quantity;
+        CustomerName = customerName;
+        Address = address;
+    }
+}
