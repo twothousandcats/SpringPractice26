215adaab2c47f28552f36c19d71c5c64b188755d
Potapenko Max <91255478+twothousandcats@users.noreply.github.com>
2026-04-27 00:26:55 +0300

    Delete OrderManager directory

--- OrderManager/OrderManager/UI/IUserInterface.cs
+++
@@ -1,8 +0,0 @@
-namespace OrderManager.UI;
-
-public interface IUserInterface
-{
-    string ReadLine(string prompt);
-    void WriteLine(string message);
-    bool AskYesNoQuestion(string prompt);
-}
