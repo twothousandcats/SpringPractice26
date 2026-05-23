3da84973016d31662d7271d17fbe59a7939fe853
Potapenko Max <mpotapenko.mail@gmail.com>
2026-05-23 16:26:03 +0300

    chore/init-solution-and-project-structure

---
+++ WebApi/WebApi/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder webApplicationBuilder = WebApplication.CreateBuilder(args);
+        webApplicationBuilder.Services.AddControllers();
+        webApplicationBuilder.Services.AddEndpointsApiExplorer();
+        webApplicationBuilder.Services.AddSwaggerGen();
+
+        WebApplication webApplication = webApplicationBuilder.Build();
+        if (webApplication.Environment.IsDevelopment())
+        {
+            webApplication.UseSwagger();
+            webApplication.UseSwaggerUI();
+        }
+
+        webApplication.UseHttpsRedirection();
+        webApplication.MapControllers();
+        webApplication.Run();
+    }
+}
